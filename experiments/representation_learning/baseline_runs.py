5b960ff3ec244c7a0aed3269c0ef7920f89ea61b
Liang Zhao <lzhao4ever@gmail.com>
2020-04-14 16:29:12 -0700

    add scripts for multiple runs of representation learning

---
+++ experiments/representation_learning/baseline_runs.py
@@ -0,0 +1,247 @@
+import sys
+sys.path.append('../..')
+
+import time
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import numpy as np
+from scipy import interpolate
+
+import random
+import tqdm
+from copy import deepcopy
+from argparse import Namespace
+import warnings
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+
+from causal_meta.utils.data_utils import RandomSplineSCM
+
+from causal_meta.utils import train_utils as tu
+from encoder import Rotor
+from causal_meta.modules.mdn import MDN, GMM, mdn_nll
+from causal_meta.modules.gmm import GaussianMixture
+
+SEED = 91023
+torch.manual_seed(SEED)
+np.random.seed(SEED)
+
+def normal(mean, std, N):
+    return torch.normal(torch.ones(N).mul_(mean), torch.ones(N).mul_(std)).view(-1, 1)
+
+def normal_like(X):
+    mean = X.mean()
+    std = X.std()
+    return normal(mean, std, X.size(0))
+
+def mlp(opt):
+    if opt.NUM_MID_LAYERS == -1:
+        return nn.Linear(opt.INP_DIM, opt.OUT_DIM)
+    else:
+        return nets.MLP(opt.INP_DIM, opt.OUT_DIM, opt.NUM_MID_LAYERS,
+                        opt.CAPACITY, opt.INP_NOISE)
+
+def mdn(opt):
+    return MDN(opt.CAPACITY, opt.NUM_COMPONENTS)
+
+def gmm(opt):
+    return GaussianMixture(opt.GMM_NUM_COMPONENTS)
+
+def xcodergen(opt):
+    # Make rotor
+    return Rotor(opt.XCODER_INIT)
+
+def plot_key(frames, key, show=True, label=None, name=None):
+    its, vals = zip(*[(frame.iter_num, getattr(frame, key)) for frame in frames])
+    if show:
+        plt.figure()
+    plt.plot(its, vals, label=label)
+    if show:
+        plt.xlabel("Iterations")
+        plt.ylabel(name if name is not None else key.title())
+        plt.show()
+
+def gradnan_filter(model):
+    nan_found = False
+    for p in model.parameters():
+        nan_mask = torch.isnan(p.grad.data)
+        nan_found = bool(nan_mask.any().item())
+        p.grad.data[nan_mask] = 0.
+    return nan_found
+
+def marginal_nll(opt, inp, nll):
+    model_g = gmm(opt)
+    if opt.CUDA:
+        model_g = model_g.cuda()
+    model_g.fit(inp, n_iter=opt.EM_ITERS)
+    with torch.no_grad():
+        loss_marginal = nll(model_g(inp), inp)
+    return loss_marginal
+
+
+def encoder_train_shared_regret(opt, model_x2y, model_y2x, scm, encoder, decoder, alpha):
+    if opt.CUDA:
+        model_x2y = model_x2y.cuda()
+        model_y2x = model_y2x.cuda()
+        encoder = encoder.cuda()
+        decoder = decoder.cuda()
+    encoder_optim = torch.optim.Adam(encoder.parameters(), opt.ENCODER_LR)
+    alpha_optim = torch.optim.Adam([alpha], opt.ALPHA_LR)
+
+    frames = np.zeros((opt.NUM_META_ITER, 2))
+    start = time.time()
+    for meta_iter in tqdm.trange(opt.NUM_META_ITER):
+        # Preheat the models
+        _ = tu.train_nll(opt, model_x2y, scm, opt.TRAIN_DISTRY, 'X2Y',
+                         mdn_nll, decoder, encoder)
+        _ = tu.train_nll(opt, model_y2x, scm, opt.TRAIN_DISTRY, 'Y2X',
+                         mdn_nll, decoder, encoder)
+        # Sample from SCM
+        X = opt.TRANS_DISTRY()
+        Y = scm(X)
+        if opt.CUDA:
+            X, Y = X.cuda(), Y.cuda()
+        # Decode
+        with torch.no_grad():
+            X, Y = decoder(X, Y)
+        # Encode
+        X, Y = encoder(X, Y)
+        with torch.no_grad():
+            if opt.USE_BASELINE:
+                baseline_y = marginal_nll(opt, Y, mdn_nll)
+                baseline_x = marginal_nll(opt, X, mdn_nll)
+            else:
+                baseline_y = 0.
+                baseline_x = 0.
+        # Save state dicts
+        state_x2y = deepcopy(model_x2y.state_dict())
+        state_y2x = deepcopy(model_y2x.state_dict())
+        # Inner loop
+        optim_x2y = torch.optim.Adam(model_x2y.parameters(), lr=opt.FINETUNE_LR)
+        optim_y2x = torch.optim.Adam(model_y2x.parameters(), lr=opt.FINETUNE_LR)
+        regrets_x2y = []
+        regrets_y2x = []
+        is_nan = False
+        # Evaluate regret discrepancy
+        for t in range(opt.FINETUNE_NUM_ITER):
+            loss_x2y = mdn_nll(model_x2y(X), Y)
+            loss_y2x = mdn_nll(model_y2x(Y), X)
+            if torch.isnan(loss_x2y).item() or torch.isnan(loss_y2x).item():
+                is_nan = True
+                break
+            optim_x2y.zero_grad()
+            optim_y2x.zero_grad()
+            loss_x2y.backward(retain_graph=True)
+            loss_y2x.backward(retain_graph=True)
+            # Filter out NaNs that might have sneaked in
+            nan_in_x2y = gradnan_filter(model_x2y)
+            nan_in_y2x = gradnan_filter(model_y2x)
+            if nan_in_x2y or nan_in_y2x:
+                is_nan = True
+                break
+            optim_x2y.step()
+            optim_y2x.step()
+            # Store for encoder
+            regrets_x2y.append(loss_x2y + baseline_x)
+            regrets_y2x.append(loss_y2x + baseline_y)
+        if not is_nan:
+            # Evaluate total regret
+            regret_x2y = torch.stack(regrets_x2y).mean()
+            regret_y2x = torch.stack(regrets_y2x).mean()
+            # Evaluate losses
+            loss = torch.logsumexp(
+                torch.stack([F.logsigmoid(alpha) + regret_x2y,
+                             F.logsigmoid(-alpha) + regret_y2x]),
+                0)
+            # Optimize
+            encoder_optim.zero_grad()
+            alpha_optim.zero_grad()
+            loss.backward()
+            # Make sure no nans
+            if torch.isnan(encoder.theta.grad.data).any():
+                encoder.theta.grad.data.zero_()
+            if torch.isnan(alpha.grad.data).any():
+                alpha.grad.data.zero_()
+            encoder_optim.step()
+            alpha_optim.step()
+            # Load original state dicts
+            model_x2y.load_state_dict(state_x2y)
+            model_y2x.load_state_dict(state_y2x)
+            # Add info
+            end = time.time()
+            frames[meta_iter,:] = [encoder.theta.item(), end-start]
+        else:
+            # Load original state dicts
+            model_x2y.load_state_dict(state_x2y)
+            model_y2x.load_state_dict(state_y2x)
+            # Add dummy info
+            end = time.time()
+            frames[meta_iter,:] = [float('nan'), end-start]
+
+    return frames
+
+def probe_xcoders(encoder, decoder):
+    # Test encoder and decoder
+    with torch.no_grad():
+        _X = torch.tensor([[1.]])
+        _Y = torch.tensor([[0.]])
+        if opt.CUDA:
+            _X, _Y = _X.to(encoder.theta.device), _Y.to(encoder.theta.device)
+        _X_d, _Y_d = decoder(_X, _Y)
+        _X_de, _Y_de = encoder(_X_d, _Y_d)
+    print(f"Initial (A, B) = {_X.item()}, {_Y.item()}")
+    print(f"Decoded (X, Y) = {_X_d.item()}, {_Y_d.item()}")
+    print(f"Encoded (U, V) = {_X_de.item()}, {_Y_de.item()}")
+
+opt = Namespace()
+
+# Model
+opt.CAPACITY = 32
+opt.NUM_COMPONENTS = 10
+opt.GMM_NUM_COMPONENTS = 10
+
+# Training
+opt.LR = 0.01
+opt.NUM_ITER = 20
+opt.NUM_META_ITER = 1000
+opt.ENCODER_LR = 0.01
+opt.ALPHA_LR = 0.001
+opt.CUDA = False #True
+opt.REC_FREQ = 10
+opt.ALPHA_INIT = 0.
+opt.USE_BASELINE = True
+
+# Fine tuning
+opt.FINETUNE_NUM_ITER = 5
+opt.FINETUNE_LR = 0.001
+opt.EM_ITERS = 500
+
+# Sampling
+opt.NUM_SAMPLES = 1000
+opt.TRAIN_DISTRY = lambda: normal(0, 2, opt.NUM_SAMPLES)
+opt.TRANS_DISTRY = lambda: normal(np.random.uniform(-4, 4),
+                                  2, opt.NUM_SAMPLES)
+opt.NUM_RUNS = 100
+
+# Encoder
+opt.DECODER_DEFAULT = -float(0.5 * np.pi/2)
+
+gt_decoder = Rotor(opt.DECODER_DEFAULT)
+rand_scm = RandomSplineSCM(False, True, 8, 8, 3, range_scale=1.)
+
+frames = np.zeros((opt.NUM_RUNS, opt.NUM_META_ITER, 2))
+for iter_run in range(opt.NUM_RUNS):
+    encoder = Rotor(0. * np.pi/2)
+    alpha = tu.make_alpha(opt)
+    model_x2y = mdn(opt)
+    model_y2x = mdn(opt)
+
+    frames[iter_run,:] = encoder_train_shared_regret(opt, model_x2y, model_y2x, rand_scm, encoder, gt_decoder, alpha)
+
+    if iter_run % 5 == 0:
+        np.save('results/baseline', frames)
---
+++ experiments/representation_learning/plots_runs.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_iter(proposed, baseline, num_transfer, file_name):
+    fig = plt.figure(figsize=(18, 12))
+    ax = plt.subplot(1, 1, 1)
+
+    ax.tick_params(axis='both', which='major', labelsize=36)
+    ax.axhline(0.5, lw=6, c='lightgray', ls='--', zorder=0)
+    ax.axhline(-0.5, lw=6, c='lightgray', ls='--', zorder=0)
+    ax.plot(proposed[:,0], lw=6, color='green', zorder=2)
+    ax.plot(baseline[:,0], lw=6, color='orange', zorder=1)
+
+    ax.set_xlim([0, num_transfer - 1])
+    ax.set_xlabel('Iterations', fontsize=48)
+    ax.set_ylabel('Encoder Angle [$\pi$/2 rad]', fontsize=48)
+    plt.savefig(file_name)
+
+def plot_time(proposed, baseline, num_transfer, file_name):
+    fig = plt.figure(figsize=(18, 12))
+    ax = plt.subplot(1, 1, 1)
+
+    ax.tick_params(axis='both', which='major', labelsize=36)
+    ax.axhline(0.5, lw=6, c='lightgray', ls='--', zorder=0)
+    ax.axhline(-0.5, lw=6, c='lightgray', ls='--', zorder=0)
+
+    ax.scatter(proposed[:num_transfer,1], proposed[:num_transfer,0], color='green', zorder=2)
+    ax.scatter(baseline[:num_transfer,1], baseline[:num_transfer,0], color='orange', zorder=1)
+
+    ax.set_xlabel('Time (sec)', fontsize=48)
+    ax.set_ylabel('Encoder Angle [$\pi$/2 rad]', fontsize=48)
+    plt.savefig(file_name)
+
+def main():
+    baseline = np.load("results/baseline.npy")
+    baseline = np.mean(baseline, axis=0)
+    baseline[:,0] = baseline[:,0] / (np.pi / 2)
+
+    proposed = np.load("results/proposed.npy")
+    proposed = np.mena(proposed, axis=0)
+    proposed[:,0] = proposed[:,0] / (np.pi / 2)
+
+    plot_iter(proposed, baseline, min(800, len(baseline[:,0])), "results/representation_runs_iter.pdf")
+    plot_time(proposed, baseline, min(800, len(baseline[:,0])), "results/representation_runs_time.pdf")
+
+if __name__ == '__main__':
+    main()
